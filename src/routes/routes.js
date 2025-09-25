28f5cf4f0e3d9bf1d0b864cfc33e4984d02ce7f1
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-09-25 16:34:59 -0300

    Removendo parâmetros das rotas balance e statement. Os dados agora vêm do payload do token JWT.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,13 +7,13 @@ const routes = (app) => {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
-    app.get("/", verifyToken, listAllUsers);
-    app.get("/balance/:id", verifyToken, getBalance);
-    app.get("/statement/:id", verifyToken, getStatement);
+    app.get("/", listAllUsers);
+    //app.get("/balance/:id", verifyToken, getBalance);
+    app.get("/balance", verifyToken, getBalance);
+    //app.get("/statement/:id", verifyToken, getStatement);
+    app.get("/statement", verifyToken, getStatement);
     app.post("/login", login);
-
     app.post("/create-user", createUser);
-
     app.post("/make-transfer", verifyToken, makeTransfer);
 
 }
