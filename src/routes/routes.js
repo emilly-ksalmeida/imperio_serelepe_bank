206eabc743c67b0f83e8508931ef455b47d96430
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-09-29 15:33:42 -0300

    Ajustes.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,21 @@
 import express from "express";
-import login from "../model/login.js";
 import verifyToken from "../middleware/verifyToken.js";
-import { listAllUsers, createUser, makeTransfer, getBalance, getStatement } from "../controller/controllers.js";
+import { listAllUsers, createUser, loginUser, makeTransfer, getBalance, getStatement } from "../controller/controllers.js";
 
 const routes = (app) => {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
     app.get("/", listAllUsers);
-    //app.get("/balance/:id", verifyToken, getBalance);
+    
     app.get("/balance", verifyToken, getBalance);
-    //app.get("/statement/:id", verifyToken, getStatement);
+   
     app.get("/statement", verifyToken, getStatement);
-    app.post("/login", login);
+
+    app.post("/login", loginUser);
+
     app.post("/create-user", createUser);
+
     app.post("/make-transfer", verifyToken, makeTransfer);
 
 }
