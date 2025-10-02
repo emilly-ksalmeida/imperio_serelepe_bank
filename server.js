04458e96b4cf480ec670d3b7a689773d2b392a9c
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-10-02 14:33:53 -0300

    Configurado cors  para teste.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import cors from "cors";
 import routes from "./src/routes/routes.js";
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST'],      
+  credentials: true                
+}));
 
 routes(app);
 
