fe9ee7868315aa4ab9a5a57168034fa3a693c761
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-10-03 15:41:57 -0300

    Adicionando as informações securityQuestion e securityAnswer ao criar um novo usuário para aplicação em recuperação de conta.

--- src/model/newUser.js
+++ src/model/newUser.js
@@ -2,10 +2,11 @@ import bcryptjs from "bcryptjs";
 import prisma from "./db.js";
 
 export default async function newUser(data) {
-  const { name, username, password, accountPassword } = data;
+  const { name, username, password, accountPassword, securityQuestion, securityAnswer } = data;
 
   const passwordHash = await bcryptjs.hash(password, 10);
   const accountPasswordHash = await bcryptjs.hash(accountPassword, 10);
+  const securityAnswerHash = await bcryptjs.hash(securityAnswer, 10);
 
   return prisma.$transaction(async (tx) => {
     const newUser = await tx.users.create({
@@ -13,6 +14,8 @@ export default async function newUser(data) {
         name,
         username,
         passwordHash,
+        securityQuestion,
+        securityAnswer: securityAnswerHash
       },
     });
     const newAccount = await tx.accounts.create({
