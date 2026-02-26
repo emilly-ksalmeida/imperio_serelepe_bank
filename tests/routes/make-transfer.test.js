7fbadb10c7c4fb7a42dbbe2a572e1ea871b932f8
Allyson Maciel Guimarães <6763464+Guepardo@users.noreply.github.com>
2026-02-26 00:42:25 -0300

    feat: enhance transfer tests to validate insufficient balance scenario

--- tests/routes/make-transfer.test.js
+++ tests/routes/make-transfer.test.js
@@ -146,12 +146,86 @@ describe("POST /make-transfer", () => {
           }
         })
 
-        console.log(updatedAdanAccount.balance, updatedMariaAccount.balance)
-        expect(updatedAdanAccount.balance).toBe("950")
-        expect(updatedMariaAccount.balance).toBe(50)
+        expect(Number(updatedAdanAccount.balance)).toBe(950)
+        expect(Number(updatedMariaAccount.balance)).toBe(50)
       })
     })
 
-    it("when the user does not have available balance", () => { })
+    describe("when the user does not have available balance", () => {
+      it("should return 400", async () => {
+        const adan = await prisma.users.create({
+          data: {
+            name: "adan",
+            username: faker.internet.username(),
+            passwordHash: await bcryptjs.hash("1234", 10),
+            securityQuestion: "abcde",
+            securityAnswer: await bcryptjs.hash("abcde", 10),
+            role: "user"
+          }
+        })
+
+        const maria = await prisma.users.create({
+          data: {
+            name: "maria",
+            username: faker.internet.username(),
+            passwordHash: await bcryptjs.hash("1234", 10),
+            securityQuestion: "abcde",
+            securityAnswer: await bcryptjs.hash("abcde", 10),
+            role: "user"
+          }
+        })
+
+        const adanAccount = await prisma.accounts.create({
+          data: {
+            balance: 1000,
+            accountPasswordHash: await bcryptjs.hash("1234", 10),
+            idUser: adan.id
+          }
+        })
+
+        const mariaAccount = await prisma.accounts.create({
+          data: {
+            accountPasswordHash: await bcryptjs.hash("1234", 10),
+            idUser: maria.id
+          }
+        })
+
+        const token = await generateJWT(adan, adanAccount.id)
+
+        const payload = {
+          toAccountId: mariaAccount.id,
+          accountPassword: '1234',
+          value: "5000"
+        }
+
+        const app = express()
+        routes(app)
+
+        const res = await request(app).post("/make-transfer")
+          .set("Authorization", `Bearer ${token}`)
+          .send(payload)
+
+
+        expect(res.statusCode).toBe(422)
+        expect(res.body).toEqual({
+          Erro: "Não existe saldo suficiente para mandar o valor $5000"
+        })
+
+        const updatedAdanAccount = await prisma.accounts.findUnique({
+          where: {
+            id: adanAccount.id
+          }
+        })
+
+        const updatedMariaAccount = await prisma.accounts.findUnique({
+          where: {
+            id: mariaAccount.id
+          }
+        })
+
+        expect(Number(updatedAdanAccount.balance)).toBe(1000)
+        expect(Number(updatedMariaAccount.balance)).toBe(0)
+      })
+    })
   })
 })
