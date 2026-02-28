3fa2b122fbf093c1353687cd55e91d8a712beb78
Allyson Maciel Guimarães <6763464+Guepardo@users.noreply.github.com>
2026-02-28 15:38:24 -0300

    refactor: move SecurityQuestionController to the auth directory and update routes accordingly

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ import SessionsController from "../controllers/auth/sessions.controller.js";
 import UsersController from "../controllers/users/users.controller.js";
 import TransactionsController from "../controllers/bank/transactions.controller.js";
 import AccountBalanceController from "../controllers/bank/account-balance.controller.js";
-import SecurityQuestionController from "../controllers/bank/security-question.controller.js";
+import SecurityQuestionController from "../controllers/auth/security-question.controller.js";
 import PasswordResetController from "../controllers/auth/password-reset.controller.js";
 
 const routes = (app) => {
@@ -20,21 +20,23 @@ const routes = (app) => {
     const securityQuestionController = new SecurityQuestionController();
     const passwordResetController = new PasswordResetController();
 
-    //Serelepepay
-    app.get("/balance", verifyToken, accountBalanceController.getBalance);
-    app.get("/statement", verifyToken, transactionsController.getStatement);
-    app.get("/user-recovery/:currentUsername", securityQuestionController.getUserSecurityQuestion);
+    // Auth routes
     app.post("/login", sessionsController.loginUser);
-    app.post("/create-user", usersController.createUser)
-    app.post("/make-transfer", verifyToken, transactionsController.makeTransfer);
+    app.get("/user-recovery/:currentUsername", securityQuestionController.getUserSecurityQuestion);
     app.post("/validate-secret-answer", securityQuestionController.validateSecretAnswer);
     app.patch("/reset-password", passwordResetController.userResetPassword);
 
-    //Serelepe Market
+    // User routes
+    app.post("/create-user", usersController.createUser)
+
+    // Bank routes
+    app.get("/balance", verifyToken, accountBalanceController.getBalance);
+    app.get("/statement", verifyToken, transactionsController.getStatement);
+    app.post("/make-transfer", verifyToken, transactionsController.makeTransfer);
+
+    // Market routes
     app.get("/products", getProducts);
-
     app.post("/create-product", createProduct);
-
     app.post("/create-purchase", verifyToken, createPurchase);
 
 }
