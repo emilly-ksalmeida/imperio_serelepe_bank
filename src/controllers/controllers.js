412e435a8b2851446a0a977a1580de8a3df558f3
Allyson Maciel Guimarães <6763464+Guepardo@users.noreply.github.com>
2026-02-28 15:34:50 -0300

    refactor: implement PasswordResetController and update routes for password reset functionality

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,29 +1,11 @@
 import { z } from "zod";
+
 import {
-  resetUserSchema,
   productSchema,
 } from "../model/validateSchema.js";
-import { resetPassword } from "../model/userRecovery.js";
+
 import { getAllProducts, newProduct } from "../model/products.js";
 import PurchaseService from "../model/purchase.js";
-
-
-// password-reset.controller.ts
-export async function userResetPassword(req, res) {
-  try {
-    const dataRecovery = req.body;
-    const validatedDataRecovery = resetUserSchema.safeParse(dataRecovery);
-    if (!validatedDataRecovery.success) {
-      const pretty = z.prettifyError(validatedDataRecovery.error);
-      throw new Error(pretty);
-    }
-    const resetResult = await resetPassword(dataRecovery);
-    res.status(200).json(resetResult);
-  } catch (erro) {
-    console.error(erro.message);
-    res.status(400).json({ Erro: erro.message });
-  }
-}
 
 //Serelepe Market
 export async function getProducts(req, res) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import verifyToken from "../middleware/verifyToken.js";
-import { userResetPassword, getProducts, createProduct, createPurchase } from "../controllers/controllers.js";
+import { getProducts, createProduct, createPurchase } from "../controllers/controllers.js";
 
 import SessionsController from "../controllers/auth/sessions.controller.js";
 import UsersController from "../controllers/users/users.controller.js";
 import TransactionsController from "../controllers/bank/transactions.controller.js";
 import AccountBalanceController from "../controllers/bank/account-balance.controller.js";
 import SecurityQuestionController from "../controllers/bank/security-question.controller.js";
+import PasswordResetController from "../controllers/auth/password-reset.controller.js";
 
 const routes = (app) => {
     app.use(express.urlencoded({ extended: true }));
@@ -17,23 +18,17 @@ const routes = (app) => {
     const transactionsController = new TransactionsController();
     const accountBalanceController = new AccountBalanceController();
     const securityQuestionController = new SecurityQuestionController();
+    const passwordResetController = new PasswordResetController();
 
     //Serelepepay
     app.get("/balance", verifyToken, accountBalanceController.getBalance);
-
     app.get("/statement", verifyToken, transactionsController.getStatement);
-
     app.get("/user-recovery/:currentUsername", securityQuestionController.getUserSecurityQuestion);
-
     app.post("/login", sessionsController.loginUser);
-
-    app.post("/create-user", usersController.createUser);
-
+    app.post("/create-user", usersController.createUser)
     app.post("/make-transfer", verifyToken, transactionsController.makeTransfer);
-
     app.post("/validate-secret-answer", securityQuestionController.validateSecretAnswer);
-
-    app.patch("/reset-password", userResetPassword);
+    app.patch("/reset-password", passwordResetController.userResetPassword);
 
     //Serelepe Market
     app.get("/products", getProducts);
