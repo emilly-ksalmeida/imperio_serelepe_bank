8662dc91e4b94a44cae377b994600ee52c81f7f8
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-03-13 11:17:48 -0300

    Adiciona estrutura base para as funções de gerenciamento de produtos por vendedor. Implementa rota de listagem de produtos por vendedor.

--- src/routes/index.js
+++ src/routes/index.js
@@ -41,6 +41,7 @@ const routes = (app) => {
     // Market routes
     app.get("/products", verifyToken, (req, res) => productsController.getProducts(req, res));
     app.post("/products", verifyToken, verifyRole, (req, res) => productsController.createProduct(req, res));
+    app.get("/products/seller/:sellerId", verifyToken, verifyRole, (req, res) => productsController.getSellerProducts(req, res));
     // app.post("/purchase", verifyToken, (req, res) => purchaseController.createPurchase(req, res));
 }
 
