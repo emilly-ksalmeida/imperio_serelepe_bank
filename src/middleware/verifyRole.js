99643ee2bc5f3d4a7cc6e729afd1b783a7dd5961
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-03-13 10:53:34 -0300

    Corrige estrutura da mensagem retornada em caso de role diferente de seller

--- src/middleware/verifyRole.js
+++ src/middleware/verifyRole.js
@@ -1,9 +1,10 @@
 const verifyRole = (req, res, next) => {
     const { role } = req.dataCurrentUser;
+    
     if(role === "seller"){
         next();
     } else {
-        res.status(403).json({ error: "Acesso negado" });
+        return res.status(403).json({ message: "Acesso negado" });
     }
 }
 
