f344f8a7cfcc8fc2773dab1437157a79e782d991
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-03-19 16:30:54 -0300

    Remove rotas não utilizadas

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.ORIGIN,
-    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PATCH", "PUT"],
     credentials: true,
   })
 );
