78f9f61e3c40bbef20cd09f57dfa3a48b51518ee
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-05-13 10:23:17 -0300

    Ajusta nome de arquivo e localização no projeto.

--- src/services/accounts/check-password.service.js
+++
@@ -1,7 +0,0 @@
-import bcryptjs from "bcryptjs";
-export default async function checkPassword(accountPassword, accountPasswordHash) {
-    return await bcryptjs.compare(
-    accountPassword,
-    accountPasswordHash
-);
-}
