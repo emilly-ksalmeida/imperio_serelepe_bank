efcc021af9508343f27e328ed24ad695d22ba3e0
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-05-14 10:18:56 -0300

    Separa o método principal em métodos privados auxiliares.

--- src/services/transfers/transfer.service.js
+++ src/services/transfers/transfer.service.js
@@ -16,22 +16,18 @@ class TransferService {
 
   async execute(data, authenticatedAccount) {
     try {
-      // Dados
       const { toAccountId, value, accountPassword } = data;
       const fromAccountId = authenticatedAccount.id;
-      // Teste de autotransferencia
+
       if (fromAccountId === toAccountId) {
         throw new Error("Não é possível realizar esta transferência.");
       }
-      // Localizando a conta do usuário autenticado
-      const fromAccount = await this.accountRepository.findAccountById(
-        this.repository,
+
+      const { fromAccount, toAccount } = await this.#validateAccounts(
         fromAccountId,
+        toAccountId,
       );
-      if (!fromAccount) {
-        throw new Error("Conta de origem não encontrada.");
-      }
-      // Validando a senha do usuario autenticado
+
       const passwordIsValid = await checkPassword(
         accountPassword,
         fromAccount.accountPasswordHash,
@@ -39,27 +35,13 @@ class TransferService {
       if (!passwordIsValid) {
         throw new Error("Senha da conta incorreta!!");
       }
-      // Verificando a existência da conta de destino
-      const toAccount = await this.accountRepository.findAccountById(
-        this.repository,
-        toAccountId,
+
+      const { newFromBalance, newToBalance } = this.#calculateBalances(
+        fromAccount,
+        value,
+        toAccount,
       );
-      if (!toAccount) {
-        throw new Error("Conta de destino não encontrada.");
-      }
-      //  Cálculo do saldo das duas contas envolvidas
-      const newFromBalance =
-        Number.parseFloat(fromAccount.balance) - Number.parseFloat(value);
-      if (newFromBalance < 0) {
-        throw new Error(
-          `Não existe saldo suficiente para mandar o valor $${value}`,
-        );
-      }
 
-      const newToBalance =
-        Number.parseFloat(toAccount.balance) + Number.parseFloat(value);
-
-      // Criando a transferencia e atualizações de saldo
       const transfer = await this.repository.$transaction(async (tx) => {
         await this.accountRepository.updateBalance(
           tx,
@@ -77,11 +59,51 @@ class TransferService {
           value,
         });
       });
+
       return transfer;
     } catch (error) {
       console.log(error);
       throw error;
     }
+  }
+  async #validateAccounts(fromAccountId, toAccountId) {
+    const fromAccount = await this.accountRepository.findAccountById(
+      this.repository,
+      fromAccountId,
+    );
+    if (!fromAccount) {
+      throw new Error("Conta de origem não encontrada.");
+    }
+    const toAccount = await this.accountRepository.findAccountById(
+      this.repository,
+      toAccountId,
+    );
+    if (!toAccount) {
+      throw new Error("Conta de destino não encontrada.");
+    }
+    return {
+      fromAccount,
+      toAccount,
+    };
+  }
+
+  #calculateBalances(fromAccount, value, toAccount) {
+    const newFromBalance =
+      Number.parseFloat(fromAccount.balance) - Number.parseFloat(value);
+
+    if (newFromBalance < 0) {
+      throw new Error(
+        `Não existe saldo suficiente para mandar o valor $${value}`,
+      );
+    }
+
+    const newToBalance =
+      Number.parseFloat(toAccount.balance) + Number.parseFloat(value);
+
+    return {
+      newFromBalance,
+      newToBalance,
+    };
   }
 }
 
