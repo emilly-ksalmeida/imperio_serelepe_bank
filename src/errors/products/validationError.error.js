de5d09b7fcb76b95ab1cc631939e63343d4e6a69
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-05-15 14:28:19 -0300

    Cria classes de erros customizadas para o fluxo de Products.

---
+++ src/errors/products/validationError.error.js
@@ -0,0 +1,8 @@
+import { AppError } from "./app.error.js";
+
+export class ValidationError extends AppError {
+  constructor(message = "Dados fora do padrão") {
+    super(message, 422);
+    this.name = "ValidationError";
+  }
+}
