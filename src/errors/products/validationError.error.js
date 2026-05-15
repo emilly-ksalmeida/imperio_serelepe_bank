ef04c7edc2ab2ecaa6d3f9a55db3de2cacb40a3e
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-05-15 15:02:42 -0300

    Corrige caminho do arquivo.

--- src/errors/products/validationError.error.js
+++ src/errors/products/validationError.error.js
@@ -1,4 +1,4 @@
-import { AppError } from "./app.error.js";
+import { AppError } from "../app.error.js";
 
 export class ValidationError extends AppError {
   constructor(message = "Dados fora do padrão") {
