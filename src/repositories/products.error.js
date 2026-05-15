8c0891e4d9cdf08c75c2613fcec491f489cb44c4
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-05-15 14:27:44 -0300

    Arquivos removidos.

--- src/repositories/products.error.js
+++
@@ -1,9 +0,0 @@
-class ProductsError extends Error {
-  constructor(message, statusCode = 500) {
-    super(message);
-    this.name = "ProductsError";
-    this.statusCode = statusCode;
-  }
-}
-
-export default ProductsError;
