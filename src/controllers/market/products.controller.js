e2921e6b6f48cc3ff960f26faed6da4e9cb47751
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-05-20 18:22:00 -0300

    Refatora o lançamento e tratamento de erros para utilizar o middleware global de erros.

--- src/controllers/market/products.controller.js
+++ src/controllers/market/products.controller.js
@@ -7,8 +7,6 @@ import {
 
 import ProductsService from "../../services/products/products.service.js";
 import { ValidationError } from "../../errors/products/validationError.error.js";
-import { AppError } from "../../errors/app.error.js";
-import { DatabaseError } from "../../errors/products/dataBaseError.error.js";
 
 class ProductsController {
   constructor(productsService = new ProductsService()) {
@@ -16,92 +14,61 @@ class ProductsController {
   }
 
   async getProducts(req, res) {
-    try {
-      const allProducts = await this.productsService.getAllProducts();
-      res.status(200).json(allProducts);
-    } catch (error) {
-      return this.#handleError(error, res);
-    }
+    const allProducts = await this.productsService.getAllProducts();
+    res.status(200).json(allProducts);
   }
 
   async getSellerProducts(req, res) {
-    try {
-      const sellerId = req.dataCurrentUser.id;
-      const sellerProducts =
-        await this.productsService.getProductsBySeller(sellerId);
-      res.status(200).json(sellerProducts);
-    } catch (error) {
-      return this.#handleError(error, res);
-    }
+    const sellerId = req.dataCurrentUser.id;
+    const sellerProducts =
+      await this.productsService.getProductsBySeller(sellerId);
+    res.status(200).json(sellerProducts);
   }
 
   async createProduct(req, res) {
-    try {
-      const productData = req.body;
-      const sellerId = req.dataCurrentUser.id;
+    const productData = req.body;
+    const sellerId = req.dataCurrentUser.id;
 
-      const validatedProductData = createProductSchema.safeParse(productData);
+    const validatedProductData = createProductSchema.safeParse(productData);
 
-      if (!validatedProductData.success) {
-        const pretty = z.prettifyError(validatedProductData.error);
-        throw new ValidationError(pretty);
-      }
-      const product = await this.productsService.newProduct({
-        ...validatedProductData.data,
-        sellerId: sellerId,
-      });
-
-      res.status(201).json(product);
-    } catch (error) {
-      return this.#handleError(error, res);
+    if (!validatedProductData.success) {
+      const pretty = z.prettifyError(validatedProductData.error);
+      throw new ValidationError(pretty);
     }
+    const product = await this.productsService.newProduct({
+      ...validatedProductData.data,
+      sellerId: sellerId,
+    });
+
+    res.status(201).json(product);
   }
 
   async updateProduct(req, res) {
-    try {
-      const { productId } = req.params;
-      const updateData = req.body;
-      const currentUserId = req.dataCurrentUser.id;
+    const { productId } = req.params;
+    const updateData = req.body;
+    const currentUserId = req.dataCurrentUser.id;
 
-      const validatedNewData = updateProductSchema.safeParse(updateData);
-      if (!validatedNewData.success) {
-        const pretty = z.prettifyError(validatedNewData.error);
-        throw new ValidationError(pretty, 422);
-      }
-      const updatedProduct = await this.productsService.updateProduct(
-        productId,
-        currentUserId,
-        updateData,
-      );
-      res.status(200).json(updatedProduct);
-    } catch (error) {
-      console.error(error);
-      return this.#handleError(error, res);
+    const validatedNewData = updateProductSchema.safeParse(updateData);
+    if (!validatedNewData.success) {
+      const pretty = z.prettifyError(validatedNewData.error);
+      throw new ValidationError(pretty, 422);
     }
+    const updatedProduct = await this.productsService.updateProduct(
+      productId,
+      currentUserId,
+      updateData,
+    );
+    res.status(200).json(updatedProduct);
   }
 
   async deleteProduct(req, res) {
-    try {
-      const { productId } = req.params;
-      const currentUserId = req.dataCurrentUser.id;
-      const _deletedProduct = await this.productsService.deleteProduct(
-        productId,
-        currentUserId,
-      );
-      res.status(204).json("Produto deletado");
-    } catch (error) {
-      return this.#handleError(error, res);
-    }
-  }
-
-  #handleError(error, res) {
-    if (error instanceof AppError) {
-      return res.status(error.statusCode).json({ error: error.message });
-    }
-    if (error instanceof DatabaseError) {
-      return res.status(500).json({ error: error.message });
-    }
-    return res.status(500).json({ error: "Erro interno do servidor." });
+    const { productId } = req.params;
+    const currentUserId = req.dataCurrentUser.id;
+    const _deletedProduct = await this.productsService.deleteProduct(
+      productId,
+      currentUserId,
+    );
+    res.status(204).json("Produto deletado");
   }
 }
 
