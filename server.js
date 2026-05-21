f5495e26d3c8239f466be833bf50c6d12ab48ce8
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-05-21 14:05:37 -0300

    Adapta o código conforme a recomendação da documentação

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-import * as Sentry from "@sentry/node";
+import "./instrument.js";
 import express from "express";
 import cors from "cors";
 import routes from "./src/routes/index.js";
 
 const app = express();
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  environment: process.env.NODE_ENV || "development",
-  tracesSampleRate: 1.0,
-});
 
 app.use(
   cors({
