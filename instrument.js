cb734d47cd0750c017727efcfde90991f95d96e5
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-05-21 15:02:03 -0300

    Arquivo removido não utilizado

--- instrument.js
+++
@@ -1,8 +0,0 @@
-import * as Sentry from "@sentry/node";
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  environment: process.env.NODE_ENV || "development",
-  tracesSampleRate: 1.0,
-  sendDefaultPii: true,
-});
