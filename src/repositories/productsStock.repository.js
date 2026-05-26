edf75a36f311519f6a43d61236ddac4a4087fe6a
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-05-26 10:03:20 -0300

    Isola as funções com interação direta com o banco de dados para validação de estoque.

---
+++ src/repositories/productsStock.repository.js
@@ -0,0 +1,50 @@
+import { NotFoundError } from "../errors/account/notFoundError.error.js";
+import { prisma, prismaImport } from "../model/db.js";
+
+class ProductsStockRepository {
+  constructor(repository = prisma) {
+    this.repository = repository;
+  }
+  async findOneById(productId) {
+    try {
+      return this.repository.products.findUnique({
+        where: { id: productId },
+        select: {
+          name: true,
+          unitPrice: true,
+          stockQuantity: true,
+        },
+      });
+    } catch (error) {
+      this.#handleDatabaseError(error);
+    }
+  }
+
+  async updateQuantityById(productId, updatedQuantity) {
+    try {
+      return this.repository.products.update({
+        where: { id: productId },
+        data: {
+          stockQuantity: updatedQuantity,
+        },
+        select: {
+          id: true,
+        },
+      });
+    } catch (error) {
+      this.#handleDatabaseError(error);
+    }
+  }
+
+  #handleDatabaseError(error) {
+    if (
+      error instanceof prismaImport.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    ) {
+      throw new NotFoundError("A operação falhou, produto não encontrado.");
+    }
+    throw error;
+  }
+}
+
+export default ProductsStockRepository;
