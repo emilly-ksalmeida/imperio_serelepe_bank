4414c614fc31746a4408cc2b765f5cf411fe169d
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-06-02 15:28:41 -0300

    Melhoria no tratamento de erros

--- src/errors/purchase/purchaseConfirmationError.error.js
+++ src/errors/purchase/purchaseConfirmationError.error.js
@@ -2,8 +2,7 @@ import { AppError } from "../app.error.js";
 
 export class PurchaseConfirmationError extends AppError {
   constructor(purchaseValidation) {
-    super("Não é possível finalizar a compra", 422);
-    this.name = "PurchaseConfirmationError";
-    this.reason = purchaseValidation;
+    super("Não é possível finalizar a compra, produto não encontrado ou estoque insuficiente", 409);
+    this.validationErrors = purchaseValidation;
   }
 }
