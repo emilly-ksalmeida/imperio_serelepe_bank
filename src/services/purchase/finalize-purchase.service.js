1db46f14d48015897a527e755d14909f483617f1
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-06-02 15:13:52 -0300

    Adiciona mais dados como parâmetro de registerOrderService.execute.

--- src/services/purchase/finalize-purchase.service.js
+++ src/services/purchase/finalize-purchase.service.js
@@ -1,22 +1,23 @@
-import { ConfirmOrderService } from "./confirmOrder.service";
-import { RegisterOrderService } from "./registerOrder.service";
-import { PurchaseConfirmationError } from "../../errors/purchase/purchaseConfirmationError.error";
+import { ConfirmOrderService } from "./confirmOrder.service.js";
+import { RegisterOrderService } from "./registerOrder.service.js";
+import { PurchaseConfirmationError } from "../../errors/purchase/purchaseConfirmationError.error.js";
 
-export class FinalizePurchaseService {
+export default class FinalizePurchaseService {
   constructor(confirmOrderService = new ConfirmOrderService(), registerOrderService = new RegisterOrderService()) {
     this.confirmOrderService = confirmOrderService;
     this.registerOrderService = registerOrderService;
   }
 
   async execute(payload) {
-    // validatedOrder pode ter propriedade success com valor true ou false
     const validatedOrder = await this.confirmOrderService.execute(payload);
 
     if (!validatedOrder.success) {
       throw new PurchaseConfirmationError(validatedOrder);
     }
 
-    const createdPurchase = await this.registerOrderService.execute(validatedOrder.details);
+    const createdPurchase = await this.registerOrderService.execute(validatedOrder.details, payload.password, payload.userAccountId);
+
+    return createdPurchase;
 
   }
 }
