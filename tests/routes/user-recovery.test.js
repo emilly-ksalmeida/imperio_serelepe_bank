478b244dbaa4130fe31e4d1b88192e62260969b7
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-08-05 15:21:06 -0300

    Refatora descrição dos testes e adiciona caso de teste para username em formato inválido.

--- tests/routes/user-recovery.test.js
+++ tests/routes/user-recovery.test.js
@@ -1,37 +1,49 @@
-import request from "supertest"
-import express from "express"
-import routes from "../../src/routes/index.js"
-import { prisma } from "../setup/prisma-cleaner.js"
-import { createUserWithAccount } from "../factories/user.factory.js"
+import request from "supertest";
+import express from "express";
+import routes from "../../src/routes/index.js";
+import { prisma } from "../setup/prisma-cleaner.js";
+import { createUserWithAccount } from "../factories/user.factory.js";
 
 describe("GET /user-recovery", () => {
-  describe("when a user try to validate secret answer", () => {
-    describe("with valid data", () => {
-      it("should return 201", async () => {
-        const { user } = await createUserWithAccount(prisma)
+  describe("When the user starts the process to see the security question", () => {
+    describe("When user enters a valid username", () => {
+      it("should return 200 and the secret question", async () => {
+        const { user } = await createUserWithAccount(prisma);
 
-        const app = express()
-        routes(app)
+        const app = express();
+        routes(app);
 
-        const res = await request(app).get(`/user-recovery/${user.username}`)
+        const res = await request(app).get(`/user-recovery/${user.username}`);
 
-        expect(res.statusCode).toBe(200)
-
+        expect(res.statusCode).toBe(200);
         expect(res.body).toEqual({
-          securityQuestion: user.securityQuestion
-        })
-      })
-    })
+          securityQuestion: user.securityQuestion,
+        });
+      });
+    });
 
-    describe("with invalid data", () => {
-      it("should return 401", async () => {
-        const app = express()
-        routes(app)
+    describe("When user enters a non-existent username", () => {
+      it("should return error status code 404", async () => {
+        const app = express();
+        routes(app);
 
-        const res = await request(app).get(`/user-recovery/invalid-username`)
+        const res = await request(app).get(`/user-recovery/nonexistentuser`);
 
-        expect(res.statusCode).toBe(404)
-      })
-    })
-  })
-})
+        expect(res.statusCode).toBe(404);
+        expect(res.body).toHaveProperty("error");
+      });
+    });
+
+    describe("When user enters a username with invalid format", () => {
+      it("should return error status code 422", async () => {
+        const app = express();
+        routes(app);
+
+        const res = await request(app).get("/user-recovery/ab");
+
+        expect(res.statusCode).toBe(422);
+        expect(res.body).toHaveProperty("error");
+      });
+    });
+  });
+});
