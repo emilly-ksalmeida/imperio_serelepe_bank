3e7483bd472563dd8b6d4a6919356952deccbd0a
emilly-ksalmeida <emilly.eksa@gmail.com>
2026-08-07 11:36:30 -0300

    Corrige limpeza do banco após cada teste.

--- tests/setup/prisma-cleaner.js
+++
@@ -1,9 +0,0 @@
-import { prisma, prismaImport } from "../../src/model/db.js"
-import { PrismaCleaner } from "@ubie/prisma-cleaner"
-
-export const cleaner = new PrismaCleaner({
-  prisma,
-  models: prismaImport.dmmf.datamodel.models,
-})
-
-export { prisma }
